2c8baa02cb88a3c6a8975d0e167c006d75e63608
quanghuy <huydqph@gmail.com>
2022-11-17 03:29:17 +0700

    validate register form

---
+++ views/admin/js/validation.js
@@ -0,0 +1,116 @@
+function validate() {
+    let name = document.querySelector(".name").value.trim();
+    let err_name = checkname(name);
+    document.querySelector(".name_err").innerText = err_name;
+
+    let email = document.querySelector(".email").value.trim();
+    let err_email = checkemail(email);
+    document.querySelector(".email_err").innerText = err_email;
+
+    let phone = document.querySelector(".phone").value.trim();
+    let err_phone = checkphone(phone);
+    document.querySelector(".phone_err").innerText = err_phone;
+
+    let pass = document.querySelector(".pass").value.trim();
+    let err_pass = checkPass(pass);
+    document.querySelector(".pass_err").innerText = err_pass;
+    
+    let confirmPass = document.querySelector(".confirm-pass").value.trim();
+    let err_confirm = checkConfirm(confirmPass);
+    document.querySelector(".ConfirmPass_err").innerText=err_confirm;
+
+}
+
+function checkname(nameValue) {
+    if (nameValue.length == 0) {
+        return "Tên không được để trống";
+    }
+
+    let arr = nameValue.split(" ");
+    let arr2 = arr.filter(function (item) {
+        return item != "";
+    })
+    if (arr2.length <= 1) {
+        return "Họ tên cần ít nhất 2 từ";
+    }
+    return "";
+}
+
+function checkemail(emailValue) {
+    if (emailValue.length == 0) {
+        return "email không được để trống"
+    }
+    if (emailValue.includes('@') == false) {
+        return "Không đúng định dạng email"
+    }
+    if (emailValue.includes('.') == false) {
+        return "Không đúng định dạng email"
+    }
+    if (emailValue.startsWith('@') == true) {
+        return "Không đúng định dạng email"
+    }
+    if (emailValue.endsWith('@') == true) {
+        return "Không đúng định dạng email"
+    }
+    if (emailValue.startsWith('.') == true) {
+        return "Không đúng định dạng email"
+    }
+    if (emailValue.endsWith('.') == true) {
+        return "Không đúng định dạng email"
+    }
+    if (chechNum(emailValue, '@', 1) == false) {
+        return "Không đúng định dạng email"
+    }
+    return "";
+}
+
+function chechNum(str, char, expected) {
+    let count = 0;
+    for (const character of str) {
+        if (character == char) {
+            count++;
+        }
+    }
+    return count == expected;
+}
+
+function checkphone(phoneValue) {
+    if (phoneValue.length == 0) {
+        return "số đt không được để trống"
+    }
+    if (!isNaN(phoneValue) == false) {
+        return " Số đt không chứa chữ"
+    }
+
+    if (phoneValue.startsWith('0') == false) {
+        return "không đúng định dạng"
+    }
+    if (phoneValue.length <= 10) {
+        return " số đt phải có ít nhất 10 số"
+    }
+    return "";
+}
+function checkPass(passValue){
+    if(passValue.length ==0){
+        return "Password không được để trống"
+    }
+    if(passValue.length < 6 ){
+        return "password có ít nhất 6 kí tự"
+    }
+    if(passValue.includes('@','.','#','^')==false){
+        return "phải có kí tự đặc biệt"
+    }
+
+    
+    if(passValue.isNaN == true){
+        return "password phải có số"
+    }
+    return "";
+}
+
+function checkConfirm(confirmValue){
+    if(confirmValue.length == 0){
+        return "Không được để trống"
+    }
+    return "";
+}
--- views/client/form/Register.php
+++ views/client/form/Register.php
@@ -6,27 +6,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/register.css">
+    <link rel="stylesheet" href="../../client/css/register.css">
     <!-- <script src="https://kit.fontawesome.com/e123c1a84c.js" crossorigin="anonymous"></script> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 </head>
+<style>
+    .input-field p{
+    text-align: left;
+    margin-left: 10px;
+    
+    }
+    .input-field label a{
+    text-decoration: none;
+    color: red;
+}
 
+.input-field .name_err{
+    font-size: 15px;
+}
+</style>
 <body>
     <div class="container">
 
         <form action="">
             <h2>Register</h2>
             <div class="input-field">
-                <input type="text" required>
-                <label for="">Enter your name</label>
+                <input type="text" required class="name"> 
+                <p style="color: red;" class="name_err"></p>
+                <label for="">Enter your name<a href="">*</a></label>
+               
             </div>
             <div class="input-field">
-                <input type="email" required>
-                <label for="">Enter your email</label>
+                <input type="text" required class="email">
+                <p style="color: red;" class="email_err"></p>
+                <label for="">Enter your email<a href="">*</a></label>
             </div>
             <div class="input-field">
-                <input type="text" required>
-                <label for="">Phone number</label>
+                <input type="text" required class="phone">
+                <p style="color: red;" class="phone_err"></p>
+                <label for="">Phone number<a href="">*</a></label>
             </div>
             <div class="gender">
                 <label class="pick" for="">Gender</label>
@@ -35,14 +53,16 @@
                 <input type="radio" name="radio"><label for="other">Other</label>
             </div>
             <div class="input-field">
-                <input type="password" class="eye" required>
-                <label for="">Password</label>
+                <input type="password" class="pass" required>
+                <p style="color: red;" class="pass_err"></p>
+                <label for="">Password<a href="">*</a></label>
                 <span class="show-btn"><i class="fas fa-eye"></i></span>
             </div>
             <div class="input-field">
-                <input type="password" class="eye" required>
-                <label for="">Confirm password</label>
-                <span class="show-btn"><i class="fas fa-eye"></i></span>
+                <input type="password" class="confirm-pass" required>
+                <p style="color: red;" class="ConfirmPass_err"></p>
+                <label for="">Confirm password<a href="">*</a></label>
+                <!-- <span class="show-btn"><i class="fas fa-eye"></i></span> -->
             </div>
             <div class="capcha">
                 <label for="capcha-input">Enter capcha</label>
@@ -51,12 +71,13 @@
                     <input type="text"  id="capcha-form" placeholder="Enter capcha">
                     <button class="capcha-refresh"><i class="fas fa-sync"></i></button>
                 </div>
+                <p style="color: red;" class="name_err"></p>
             </div>
             <!-- <div class="forgot">
                 <a href="./ForgotPass.html">Forgot password?</a>
             </div> -->
             <div class="button">
-                <button style="--clr:#1e9bff">
+                <button style="--clr:#1e9bff" onclick="validate()">
                     <a href=""><span>Register Now</span></a>
                 </button>
             </div>
@@ -70,16 +91,19 @@
 </html>
 <!-- <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script> -->
-<script src=""></script>
+<script src="../../admin/js/validation.js"></script>
 <script>
-    const passField = document.querySelector(".eye");
+    const passField = document.querySelector(".pass");
+    const passConfirm=document.querySelector(".confirm-pass");
     const showBtn = document.querySelector("span i");
     showBtn.onclick = (() => {
         if (passField.type == "password") {
             passField.type = "text";
+            passConfirm.type = "text";
             showBtn.classList.add("hide-btn");
         } else {
             passField.type = "password";
+            passConfirm.type ="password";
             showBtn.classList.remove("hide-btn");
         }
     });
