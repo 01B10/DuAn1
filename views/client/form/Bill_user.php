afca4bb2c137655ffa6615416e84c52679ec3577
Quanghwi <huydqph24532@fpt.edu.vn>
2022-11-21 17:18:50 +0700

    bill user

---
+++ views/client/form/Bill_user.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hóa đơn</title>
+    <link rel="stylesheet" href="../assets/css/Bill_user.css">
+</head>
+<body>
+    <main>
+        <h2>Hóa đơn</h2>
+        <table cellpadding="10" >
+            <thead>
+                <th>Tên tour</th>
+                <th>Tên khách hàng</th>
+                <th>Thời gian đặt</th>
+                <th>Số người lớn</th>
+                <th>Số trẻ em</th>
+                <th>Ngày khởi hành</th>
+                <th>Điểm khởi hành</th>
+                <th>Tổng tiền</th>
+                <th>Phương thức thanh toán</th>
+                <th>Trạng thái</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <span>du lịch hà nội
+                            hjavdafha
+                            kbadfbiadb
+                        </span>
+                        <!-- php code -->
+                    </td>
+                    <td>
+                       <span>Đặng Quang HUy</span>
+                        <!-- php code -->
+                    </td>
+                    <td>
+                        <input class="datetime" type="datetime-local" name="" id="">
+                        <!-- php code -->
+                    </td>
+                    <td>
+                        <span>1</span>
+                        <!-- php code -->
+                    </td>
+                    <td>
+                        <span>1</span>
+                        <!-- php code -->
+                    </td>
+                    <td>
+                        <input type="date" name="" id="">
+                        <!-- php code -->
+                    </td>
+                    <td>
+                        <span>Thái Bình</span>
+                        <!-- php code -->
+                    </td>
+                    <td>
+                        <span>190.000.000đ</span>
+                        <!-- php code -->
+                    </td>
+                    <td>
+                        <span>Thanh toán online</span>
+                        <!-- php code -->
+                    </td>
+                    <td>
+                        <a href=""><button>Đã xong</button></a>
+                        <!-- php code -->
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+</body>
+</html>
